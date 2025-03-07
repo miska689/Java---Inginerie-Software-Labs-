3a87473cf6c8a7eaeba5d2e22dffb1d3259ada03
Mihail Boinceanu <mihaiboinceanu2004@gmail.com>
2025-03-07 22:08:01 +0200

    Lab3 - Exercitiul 11: TicTacToe | Create template

---
+++ src/main/java/org/example/lab3/TicTacToe.java
@@ -0,0 +1,38 @@
+package org.example.lab3;
+
+public class TicTacToe {
+  private int size;
+  private int[][] board;
+  private boolean playerTurn;
+
+  public TicTacToe(int size) {
+    this.size = size;
+    this.board = new int[size][size];
+  }
+
+  private void printBoard() {
+    // TODO: Print the board
+  }
+
+  private boolean checkWin() {
+    // TODO: Check if there is a win
+    return false;
+  }
+
+  private boolean checkDraw() {
+    // TODO: Check if there is a draw
+    return false;
+  }
+
+  private void playerMove() {
+    // TODO: Player move
+  }
+
+  private void computerMove() {
+    // TODO: Computer move
+  }
+
+  public void runGame() {
+    // TODO: Run the game
+  }
+}
