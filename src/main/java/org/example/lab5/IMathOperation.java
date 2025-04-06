55e157e2c73b95c66b7a96bf4459b618bd19c917
Mihail Boinceanu <mihaiboinceanu2004@gmail.com>
2025-04-06 19:18:03 +0300

    Lab5 - Ex1: MathClass

---
+++ src/main/java/org/example/lab5/IMathOperation.java
@@ -0,0 +1,14 @@
+package org.example.lab5;
+
+public interface IMathOperation {
+
+  double add(double a, double b);
+
+  double sub(double a, double b);
+
+  double mul(double a, double b);
+
+  double div(double a, double b);
+
+  double pow(double a, double b);
+}
---
+++ src/main/java/org/example/lab5/MathClass.java
@@ -0,0 +1,48 @@
+package org.example.lab5;
+
+import org.example.Main;
+
+@FunctionalInterface
+interface FIAdd {
+  double add(double a, double b);
+}
+
+@FunctionalInterface
+interface FISub {
+  double sub(double a, double b);
+}
+
+@FunctionalInterface
+interface FIMul {
+  double mul(double a, double b);
+}
+
+@FunctionalInterface
+interface FIDiv {
+  double div(double a, double b);
+}
+
+public class MathClass implements FIAdd, FISub, FIMul, FIDiv {
+
+  public MathClass() {}
+
+  @Override
+  public double add(double a, double b) {
+    return a + b;
+  }
+
+  @Override
+  public double sub(double a, double b) {
+    return a - b;
+  }
+
+  @Override
+  public double mul(double a, double b) {
+    return a * b;
+  }
+
+  @Override
+  public double div(double a, double b) {
+    return a / b;
+  }
+}
